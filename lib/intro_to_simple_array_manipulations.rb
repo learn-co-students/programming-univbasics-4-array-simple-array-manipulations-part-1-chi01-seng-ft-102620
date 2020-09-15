d22146e35526b102c22edf58b75eabda52d67b8d
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-15 23:41:19 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow << next_color
+end
+
+
+def using_unshift(arr, string)
+  arr.unshift(string)
+end
+
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
